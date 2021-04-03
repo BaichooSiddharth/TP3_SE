f18f5fec7c1210f06b964e727ffb82de03029972
Siddharth <sidh.baichoo@gmail.com>
2021-04-03 02:32:41 -0400

    changements pm.c

--- src/pm.c
+++ src/pm.c
@@ -24,27 +24,61 @@ void pm_init (FILE *backing_store, FILE *log)
 void pm_download_page (unsigned int page_number, unsigned int frame_number)
 {
   download_count++;
-  /* ¡ TODO: COMPLÉTER ! */
+  unsigned int count = 0;
+  fpos_t pos;
+  fgetpos(pm_backing_store, &pos);
+  int char_read;
+  unsigned int start_pm = frame_number * PAGE_FRAME_SIZE;
+  unsigned int start_bs = page_number * PAGE_FRAME_SIZE;
+  unsigned int end_bs = page_number+1 * PAGE_FRAME_SIZE;
+  unsigned int size_backing_store = NUM_PAGES * PAGE_FRAME_SIZE;
+  while(count < size_backing_store && count < end_bs){
+      char_read = fgetc(pm_backing_store);
+      if(count >= start_bs){
+          pm_memory[start_pm + (count - start_bs)] = char_read;
+      }
+      count++;
+  }
+  fsetpos(pm_backing_store, &pos);
 }
 
 // Sauvegarde la frame spécifiée dans la page du backing store
 void pm_backup_page (unsigned int frame_number, unsigned int page_number)
 {
   backup_count++;
+  /*Portion du code pour faire un log
+  //on fait un tableau de char pour sauvegarder le frame number et le page number
+  int size_fr = log10(frame_number);
+  int size_pg = ;
+  char fr_n[2];
+  char pg_n[3];
+  char log_line[7];
+   */
+  char frame[PAGE_FRAME_SIZE];
+  fpos_t pos;
+  fgetpos(pm_backing_store, &pos);
+  unsigned int start_bs = page_number * PAGE_FRAME_SIZE;
+  unsigned int start_pm = frame_number * PAGE_FRAME_SIZE;
+  for(int i=0; i<PAGE_FRAME_SIZE; i++){
+      frame[i] = pm_memory[start_pm+i];
+  }
+  fseek(pm_backing_store, start_bs, SEEK_SET);
+  fwrite(frame, PAGE_FRAME_SIZE, 1,pm_backing_store);
+  fsetpos(pm_backing_store, &pos);
   /* ¡ TODO: COMPLÉTER ! */
 }
 
 char pm_read (unsigned int physical_address)
 {
   read_count++;
-  /* ¡ TODO: COMPLÉTER ! */
-  return '!';
+  return pm_memory[physical_address];
 }
 
 void pm_write (unsigned int physical_address, char c)
 {
   write_count++;
   /* ¡ TODO: COMPLÉTER ! */
+  pm_memory[physical_address] = c;
 }
 
 
